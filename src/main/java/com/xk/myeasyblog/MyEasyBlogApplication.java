24ad81239d03024a04b5fde09b17dccb1f533002
Boom <847290082@qq.com>
2022-01-02 16:01:09 +0800

    项目初始化

---
+++ src/main/java/com/xk/myeasyblog/MyEasyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.xk.myeasyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEasyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEasyBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xk/myeasyblog/MyEasyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xk.myeasyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEasyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
